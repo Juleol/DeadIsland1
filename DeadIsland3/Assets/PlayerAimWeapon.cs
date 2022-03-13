f4d594935939924194f36a521e6993b7b60abf7a
Juleol <julio.carvajal@elev.cybergymnasiet.se>
2022-03-13 08:55:52 +0100

    shoot (uncompete)

---
+++ DeadIsland3/Assets/PlayerAimWeapon.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerAimWeapon : MonoBehaviour
+{
+    private Transform aimTransform;
+    private Animator aimAnimator;
+    private void Awake()
+    {
+        aimTransform = transform.Find("Aim");
+        aimAnimator = aimTransform.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        HandleAiming();
+        HandleShooting();
+    }
+    private void HandleShooting()
+    {
+        Vector3 mousePosition = World.GetMouseWorldPosition();
+
+        Vector3 aimDirection = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+    }
+    private void HandleAiming()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            aimAnimator.SetTrigger("Shoot");
+        }
+    }
+}
+
